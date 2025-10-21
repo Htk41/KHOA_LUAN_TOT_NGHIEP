5a2f78bdbf480db636b3f2a1385140cda6ab2145
Hoang Kiet <hoangkiet4104@gmail.com>
2025-10-21 12:28:10 +0700

    Thêm api cho airplane, airport, city

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ const apiRoutes = require('./routes');
 
 const app = express();
 
+app.use(express.json()); //pass incoming resquest body
+app.use(express.urlencoded({extended: true}));
+
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server in PORT : ${ServerConfig.PORT}`);
 });
 
---
+++ src/utils/common/error-response.js
@@ -0,0 +1,8 @@
+const error = {
+    success: false,
+    message: 'Somethingwent wrong',
+    data: {},
+    error: {}
+}
+
+module.exports = error;
